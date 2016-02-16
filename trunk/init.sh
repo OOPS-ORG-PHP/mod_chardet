f44b94be93270245453974613680cfad5ed85ad4
oops <oops@d2459aca-65cc-11e0-ae59-849fcd928d9c>
2016-02-16 18:31:52 +0000

    remove tests files

--- trunk/init.sh
+++ trunk/init.sh
@@ -7,4 +7,4 @@ fi
 rm -rf autom4te.cache/ build/ include/ modules/ tags
 rm -f Makefile.global ac*.m4 config.guess config.h* config.nice config.sub configure*
 rm -f install-sh ltmain.sh missing mkinstalldirs scan_makefile_in.awk run-tests.php .deps
-
+rm -f tests/*.{diff,exp,log,out,php,sh,mem}
