456d22e53393a6d48792774011de6e59ea9ac593
JoungKyun Kim <joungkyun@gmail.com>
2020-05-25 01:55:09 +0900

    enhanced distribute fucntions

--- init.sh
+++ init.sh
@@ -1,9 +1,68 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -f Makefile ]; then
-	make distclean
-fi
-rm -rf autom4te.cache/ build/ include/ modules/ tags
-rm -f Makefile.global ac*.m4 config.guess config.h* config.nice config.sub configure*
-rm -f install-sh ltmain.sh missing mkinstalldirs scan_makefile_in.awk run-tests.php .deps
-rm -f tests/*.{diff,exp,log,out,php,sh,mem}
+source /usr/share/annyung-release/functions.d/bash/functions
+
+errmsg () {
+	echo "$*" > /dev/stderr
+}
+
+usage () {
+	echo "Usage: $0 [clean|pack]"
+	exit 1
+}
+
+opts=$(getopt -u -o h -l help -- "$@")
+[ $? != 0 ] && usage
+
+set -- ${opts}
+for i
+do
+	case "$i" in
+		-h|--help)
+			usage
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+	esac
+done
+
+case "${1}" in
+	clean)
+		[ -f Makefile ] && make distclean
+		rm -rf include autom4te* build modules tags
+		rm -f Makefile.g* Makefile.f* Makefile.o* Makefile
+		rm -f config.log config.nice config.g* config.s* config.h*
+		rm -f ac* configure* mkinstalldirs .deps missing ltmain.sh
+		rm -f run-tests.php install* libtool
+		rm -f tests/*.{diff,exp,log,out,php,sh,mem}
+
+		rm -f package.xml
+		;;
+	pack)
+		cp -af package.xml.tmpl package.xml
+		list=$(grep "md5sum" ./package.xml | sed -r 's/.*"@|@".*//g')
+
+		for i in ${list}
+		do
+			md5s=$(md5sum "$i" | awk '{print $1}')
+			perl -pi -e "s!\@${i}\@!${md5s}!g" ./package.xml
+		done
+
+		curdate=$(date +'%Y-%m-%d')
+		curtime=$(date +'%H:%M:%S')
+
+		perl -pi -e "s!\@curdate\@!${curdate}!g" ./package.xml
+		perl -pi -e "s!\@curtime\@!${curtime}!g" ./package.xml
+
+		pecl package
+		rm -f package.xml
+		;;
+	*)
+		errmsg "Unsupport mode '${1}'"
+		exit 1
+esac
+
+exit 0
